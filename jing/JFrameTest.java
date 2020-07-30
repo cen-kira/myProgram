7fd4981f6fdd6a407d27329492596becb3825968
cen-kira <68886652+cen-kira@users.noreply.github.com>
2020-07-30 11:55:25 +0800

    Add files via upload

--- jing/JFrameTest.java
+++ jing/JFrameTest.java
@@ -4,10 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class JFrameTest extends JFrame
 {
     TestPane tp = null;
+
     public static void main(String[] args)
     {
         new JFrameTest();
@@ -15,15 +18,21 @@ public class JFrameTest extends JFrame
     }
     public JFrameTest()
     {
+
         tp = new TestPane();
         this.add(tp);
+        this.addKeyListener(tp);
+        this.setSize(300,400);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
     }
-    class TestPane extends JPanel
+    class TestPane extends JPanel implements KeyListener
     {
         int x,y;
+        int x1 = 50;
+        int y1 = 50;
         int m=1;
         int n=1;
         public TestPane()
@@ -54,13 +63,45 @@ public class JFrameTest extends JFrame
         }
 
         @Override
-        public void paint(Graphics g,Graphics2D g2d)
+        public void paint(Graphics g)
         {
+            Graphics2D g2d = (Graphics2D) g;
             super.paint(g);
             super.paint(g2d);
             g.setColor(Color.RED);
             g.fillOval(x,y,30,30);
+            g2d.setColor(Color.BLACK);
+            g2d.fillOval(x1,y1,30,30);
+
     }
+        public void keyPressed(KeyEvent arg0)
+        {
+            if(arg0.getKeyCode()==KeyEvent.VK_UP)
+            {
+                y1-=20;
+            }
+            if(arg0.getKeyCode()==KeyEvent.VK_DOWN)
+            {
+                y1+=20;
+            }
+            if(arg0.getKeyCode()==KeyEvent.VK_LEFT)
+            {
+                x1-=20;
+            }
+            if(arg0.getKeyCode()==KeyEvent.VK_RIGHT)
+            {
+                x1+=20;
+            }
+            this.repaint();
+        }
+        public void keyReleased(KeyEvent arg0)
+        {
+
+        }
+        public void keyTyped(KeyEvent arg0)
+        {
+
+        }
 
     }
 }
